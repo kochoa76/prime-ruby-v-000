9a529964ec0235f575dc88297bff6b04d490e3e2
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-14 17:28:16 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 # Add  code here!
 def prime?(num)
-  if num<0 || num ==0 || num ==1
+  if num < 0 || num ==0 || num ==1
     return false
   else
-    (2..num).to_a.all? {|i| num % i }
-    return true 
+    (2..num).to_a.all? {|i| num % i !=0  }
+    return true
 end
 end
